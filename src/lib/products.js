8c7d91f50445df6cf948fecdb96f556a3ac7b24c
robotson <robotsun@gmail.com>
2023-04-02 19:53:01 -0500

    apprently you can't have a nextjs route that doesn't return a react component so i just used fs to save a json file of the products in the public folder. it's ya boi

--- src/lib/products.js
+++ src/lib/products.js
@@ -1,18 +1,36 @@
 import Stripe from 'stripe';
+import fs from 'fs';
+import path from 'path';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function getProducts() {
-    const products = await stripe.products.list({active:true});
+    try {
+        const products = await stripe.products.list({active:true});
 
-    for(let i = 0; i < products.data.length; i++){
-        const price = await stripe.prices.list({
-                                product: products.data[i].id,
-                                active: true,
-                                limit: 1,
-                            });
-        products.data[i]['price'] = price.data[0];
+        for(let i = 0; i < products.data.length; i++){
+            const price = await stripe.prices.list({
+                                    product: products.data[i].id,
+                                    active: true,
+                                    limit: 1,
+                                });
+            products.data[i]['price'] = price.data[0];
+        }
+        console.log(process.cwd())
+        const publicPath = path.join(process.cwd(), 'public');
+        const dataDir = path.join(publicPath, 'data');
+        if (!fs.existsSync(dataDir)) {
+          fs.mkdirSync(dataDir);
+        }
+        const productsDataFile = path.join(dataDir, 'products.json');
+        console.log(productsDataFile)
+        fs.writeFileSync(productsDataFile, JSON.stringify(products.data));
+    
+        return products.data;
     }
-    return products.data;
+    catch (error) {
+        console.error(error);
+        return []; // Return null or an empty array to indicate that an error occurred
+    }
 }
   
