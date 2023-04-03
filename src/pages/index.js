6010dbada9d0ec80499affeec43fa15f8e675fef
robotson <robotsun@gmail.com>
2023-04-02 20:40:57 -0500

    messing around wiht some simple cart code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,38 @@
 import Head from 'next/head';
 import { getProducts } from '@/lib/products';
 import currency from 'currency.js';
+import { useState, useEffect } from 'react';
 
 export default function ItsYaBoi({ allProducts }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem('cartItems');
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []);
+
+  const handleAddToCart = (product) => {
+    const existingItem = cartItems.find((item) => item.id === product.id);
+    if (existingItem) {
+      // If the item is already in the cart, update the quantity
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      // If the item is not in the cart, add it with a quantity of 1
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  useEffect(() => {
+    // Store the cart items in local storage whenever the cartItems state changes
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
+
   return (
     <>
       <Head>
@@ -15,10 +45,25 @@ export default function ItsYaBoi({ allProducts }) {
       <ul>
           {allProducts.map((product) => (
           <li key={product.id}>
-            {product.name}{", "}{currency(product.price.unit_amount, { fromCents: true }).format()}
+            {product.name}{", "}{currency(product.price.unit_amount, { fromCents: true }).format()} 
+            <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
           </li>
         ))}
       </ul>
+
+
+      <h1>Your cart:</h1>
+      {cartItems.length > 0 ? (
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              {item.name} x {item.quantity}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>(cart is empty)</p>
+      )}
     </>
   )
 }
