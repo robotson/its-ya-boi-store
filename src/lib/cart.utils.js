2ed0854eb100a2148aaddb7596c18310b2b0407b
robotson <robotsun@gmail.com>
2023-04-04 16:15:06 -0500

    humiliating level of commits to test 2

--- src/lib/cart.utils.js
+++ src/lib/cart.utils.js
@@ -45,6 +45,7 @@ export function addToCart(product, cartItems, setCartItems) {
       .then(response => {
           // Handle redirect response
           const data = response.json();
+          console.log(data);
           window.location.href = data.data.url;
       })
       .catch(error => {
