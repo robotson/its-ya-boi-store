dd775dc984adfdf6d264d57914765d28b5af86c0
robotson <robotsun@gmail.com>
2023-04-04 01:36:08 +0000

    Deploying to gh-pages from @ robotson/its-ya-boi-store@d03ffecaf5f7ab4e78075db34843f71fe75aa924 🚀

---
+++ _next/static/-usqD6nfnSzPAPvqh0qQh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ea4b679ffbfef7a0.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
